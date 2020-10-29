6f491d9fc5a8678d13a216c4e49e7163c773528c
danielortiz3 <danielortiz3@gmail.com>
2020-10-29 01:19:53 -0300

    Removed useTheme from styled-components.

--- mobile/src/pages/Profile/index.tsx
+++ mobile/src/pages/Profile/index.tsx
@@ -12,7 +12,6 @@ import { useNavigation } from '@react-navigation/native';
 import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
-import { useTheme } from 'styled-components';
 import Icon from 'react-native-vector-icons/Feather';
 
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -40,7 +39,6 @@ interface ProfileFormData {
 
 const Profile: React.FC = () => {
   const { user, updateUser } = useAuth();
-  const theme = useTheme();
 
   const formRef = useRef<FormHandles>(null);
   const emailInputRef = useRef<TextInput>(null);
